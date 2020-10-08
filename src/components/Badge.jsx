0d1d181fd68d1cfbe2963e8bc11548e6fc725182
Nico <nmanograsso@gmail.com>
2020-10-08 09:13:32 -0300

    add the first component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,24 +1,29 @@
 import React from "react";
+import "../styles/components/Badge.scss";
 import confLogo from "../images/badge-header.svg";
 
 const Badge = () => (
-  <>
-    <div>
+  <div className="badge">
+    <div className="badge__header">
       <img src={confLogo} alt="logo of component" />
-      <div>
-        <img src="https://en.gravatar.com/avatar?d=identicon" alt="Avatar" />
-        <h1>
-          Nicolas <br />
-          Manograsso
-        </h1>
-      </div>
-      <h2>Engeneer Manager</h2>
-      <p>@NicoMano</p>
-      <div>
-        <p>#PlatziConf</p>
-      </div>
     </div>
-  </>
+    <div className="badge__section-name">
+      <img
+        className="badge__avatar"
+        src="https://en.gravatar.com/avatar?d=identicon"
+        alt="Avatar"
+      />
+      <h1>
+        Nicolas <br />
+        Manograsso
+      </h1>
+    </div>
+    <div className="badge__section-info">
+      <h4>Engeneer Manager</h4>
+      <p>@NicoMano </p>
+    </div>
+    <p>#PlatziConf</p>
+  </div>
 );
 
 export default Badge;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,21 +1,9 @@
 import React from "react";
 import Badge from "../components/Badge";
-import logo from "../logo.svg";
 import "./App.css";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        Learn React
-      </header>
-      <Badge />
-    </div>
-  );
+  return <Badge />;
 };
 
 export default App;
