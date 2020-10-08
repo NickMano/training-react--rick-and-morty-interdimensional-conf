e12c687eaa53041972036c521d94816862047645
Nico <nmanograsso@gmail.com>
2020-10-08 09:27:01 -0300

    add props to component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,28 +2,31 @@ import React from "react";
 import "../styles/components/Badge.scss";
 import confLogo from "../images/badge-header.svg";
 
-const Badge = () => (
-  <div className="badge">
-    <div className="badge__header">
-      <img src={confLogo} alt="logo of component" />
+const Badge = (props) => {
+  const { firstName, lastName, jobTitle, twitter } = props;
+  return (
+    <div className="badge">
+      <div className="badge__header">
+        <img src={confLogo} alt="logo of component" />
+      </div>
+      <div className="badge__section-name">
+        <img
+          className="badge__avatar"
+          src="https://en.gravatar.com/avatar?d=identicon"
+          alt="Avatar"
+        />
+        <h1>
+          {firstName} <br />
+          {lastName}
+        </h1>
+      </div>
+      <div className="badge__section-info">
+        <h4>{jobTitle}</h4>
+        <p>@{twitter}</p>
+      </div>
+      <p>#PlatziConf</p>
     </div>
-    <div className="badge__section-name">
-      <img
-        className="badge__avatar"
-        src="https://en.gravatar.com/avatar?d=identicon"
-        alt="Avatar"
-      />
-      <h1>
-        Nicolas <br />
-        Manograsso
-      </h1>
-    </div>
-    <div className="badge__section-info">
-      <h4>Engeneer Manager</h4>
-      <p>@NicoMano </p>
-    </div>
-    <p>#PlatziConf</p>
-  </div>
-);
+  );
+};
 
 export default Badge;
--- src/pages/App.js
+++ src/pages/App.js
@@ -3,7 +3,14 @@ import Badge from "../components/Badge";
 import "./App.css";
 
 const App = () => {
-  return <Badge />;
+  return (
+    <Badge
+      firstName="Nicolas"
+      lastName="Manograsso"
+      jobTitle="Team Manager camino a EM"
+      twitter="NicoMano"
+    />
+  );
 };
 
 export default App;
