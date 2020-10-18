a9be0ea5dd5fa116c42f0015f83403da28ccd815
Nico <nmanograsso@gmail.com>
2020-10-18 09:04:48 -0300

    Add loading state

--- src/components/BadgeCompact/BadgeCompact.jsx
+++ src/components/BadgeCompact/BadgeCompact.jsx
@@ -4,23 +4,37 @@ import "./BadgeCompact.scss";
 const BadgeCompact = (props) => {
   const cssClassName = "BadgeCompact";
 
-  const { firstName, lastName, jobTitle, twitter, avatar } = props;
-  return (
+  const { firstName, lastName, jobTitle, twitter, avatar, loading } = props;
+
+  if (!loading) {
+    return (
+      <div className={`${cssClassName}`}>
+        <img
+          className={`${cssClassName}__avatar`}
+          src={avatar ? avatar : "https://en.gravatar.com/avatar?d=identicon"}
+          alt="Avatar"
+        />
+        <div className={`${cssClassName}__section-name`}>
+          <h4 className={`${cssClassName}__section-name--name`}>
+            {firstName} {lastName}
+          </h4>
+          <h5>{jobTitle}</h5>
+          <p className="p--twitter">@{twitter}</p>
+        </div>
+      </div>
+    );
+  } else {
+    return(
     <div className={`${cssClassName}`}>
-      <img
-        className={`${cssClassName}__avatar`}
-        src={avatar ? avatar : "https://en.gravatar.com/avatar?d=identicon"}
-        alt="Avatar"
-      />
+      <div className={`${cssClassName}__avatar loading`} />
       <div className={`${cssClassName}__section-name`}>
-        <h4 className={`${cssClassName}__section-name--name`}>
-          {firstName} {lastName}
-        </h4>
-        <h5>{jobTitle}</h5>
-        <p className="p--twitter">@{twitter}</p>
+        <div className={`${cssClassName}__section-name--name loading`} />
+        <div className={`${cssClassName}__section-name--jobTitle loading`} />
+        <div className={`${cssClassName}__section-name--twitter loading`} />
       </div>
     </div>
-  );
+    );
+  }
 };
 
 export default BadgeCompact;
--- src/pages/Badges/Badges.jsx
+++ src/pages/Badges/Badges.jsx
@@ -4,15 +4,19 @@ import Hero from "../../components/Hero/Hero";
 import BadgeList from "../../components/BadgeList/BadgeList";
 import { Link } from "react-router-dom";
 import fetchCharacter from "../../components/utils/fetchCharacter";
+import BadgeCompact from "../../components/BadgeCompact/BadgeCompact";
 
 const Badges = () => {
   const [characters, setCharacters] = useState([])
   const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
 
   useEffect( () => {
+    setLoading(true);
     fetchCharacter(characters, page)
     .then( data => {
       setCharacters(data)
+      setLoading(false)
     }
     )
   }, [page]);
@@ -29,6 +33,7 @@ const Badges = () => {
         </div>
         <div className={`${cssClassName}__badgeList`} >
           <BadgeList badges={characters}/>
+          {loading && <BadgeCompact loading />}
           <button onClick= {() => setPage(page+1)} className={`btn btn--terciary btn--full-width`}>Load more</button>
         </div>
       </div>
